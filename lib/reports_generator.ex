244b95a8e3245aa37777873c2e40afbdb99456af
Gabriel Rozzetti <rozzetti.matheus@gmail.com>
2022-01-20 15:09:33 -0300

    add build_from_many function and tests

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,6 +20,19 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Provide a list of filenames"}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     higher_cost = Enum.max_by(report[option], fn {_key, value} -> value end)
     {:ok, higher_cost}
@@ -27,7 +40,7 @@ defmodule ReportsGenerator do
 
   def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = _report) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
@@ -36,13 +49,29 @@ defmodule ReportsGenerator do
     # |> Map.put("users", users)
     # |> Map.put("foods", foods)
 
-    %{report | "users" => users, "foods" => foods}
+    # %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp sum_reports(
+         %{"foods" => foods1, "users" => users1},
+         %{"foods" => foods2, "users" => users2}
+       ) do
+    # merge maps
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2), do: Map.merge(map1, map2, fn _key, v1, v2 -> v1 + v2 end)
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"foods" => foods, "users" => users}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -99,4 +99,77 @@ defmodule ReportsGeneratorTest do
       assert response == expected_response
     end
   end
+
+  describe "build_from_many/1" do
+    test "when a filelist is provided, builds the report" do
+      # Setup
+      filenames = ["report_test.csv", "report_test.csv"]
+
+      # Exercise
+      response = ReportsGenerator.build_from_many(filenames)
+
+      expected_response =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 2,
+             "churrasco" => 4,
+             "esfirra" => 6,
+             "hambúrguer" => 4,
+             "pastel" => 0,
+             "pizza" => 4,
+             "prato_feito" => 0,
+             "sushi" => 0
+           },
+           "users" => %{
+             "1" => 96,
+             "10" => 72,
+             "11" => 0,
+             "12" => 0,
+             "13" => 0,
+             "14" => 0,
+             "15" => 0,
+             "16" => 0,
+             "17" => 0,
+             "18" => 0,
+             "19" => 0,
+             "2" => 90,
+             "20" => 0,
+             "21" => 0,
+             "22" => 0,
+             "23" => 0,
+             "24" => 0,
+             "25" => 0,
+             "26" => 0,
+             "27" => 0,
+             "28" => 0,
+             "29" => 0,
+             "3" => 62,
+             "30" => 0,
+             "4" => 84,
+             "5" => 98,
+             "6" => 36,
+             "7" => 54,
+             "8" => 50,
+             "9" => 48
+           }
+         }}
+
+      # Assertion
+      assert response == expected_response
+    end
+
+    test "when a list is not provided, gives an error" do
+      # Setup
+      filenames = "banana"
+
+      # Exercise
+      response = ReportsGenerator.build_from_many(filenames)
+
+      expected_response = {:error, "Provide a list of filenames"}
+
+      # Assertion
+      assert response == expected_response
+    end
+  end
 end
